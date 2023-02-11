a312b8c138c904c20ee01a5d39ee0b0e5e3a1179
Matheus Nunes Puppe <matheus.puppe90@hotmail.com>
2023-02-11 19:22:43 -0300

    Update script.js
    remove console.log

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("myButton").addEventListener("click", function() {
     let currentUrl = window.location.href;
-    console.log('teste');
-    console.log(currentUrl);
     let jumpPaywall = "https://12ft.io/proxy?q=" + currentUrl;
     window.location.replace(jumpPaywall);
   });
